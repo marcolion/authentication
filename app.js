3f9408a79fd75e260b1eb50f1f8b88c09cda97cd
marcolion <marcoleao@icloud.com>
2022-02-10 14:11:07 -0500

    implementing bcrypt

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -27,8 +26,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 ////////////////////////////////////////////////////////////////////////////////////
@@ -48,16 +45,19 @@ app.get("/register", function(req, res){
 
 //-------To create a Register Route--------------------
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if (err){
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 //-----------------------------------------------------
@@ -72,9 +72,11 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if (foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result == true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
